7145f969505e0477901fb75b6bff66c6ae9267ee
Angelica <a.cortes.godoy@gmail.com>
2022-07-31 16:04:00 -0400

    mi first commit

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+        const tareaInput = document.querySelector("#nuevaTarea")
+        const btnAgregar = document.querySelector("#agregarTarea")
+        const listaDeTareas = document.querySelector("#listaDeTareas")
+        const tbody = document.querySelector("tbody")
+        const cuenta = document.querySelector("#cuenta-tareas")
+
+        const tareas = []
+
+        function renderTareas () {
+            tbody.innerHTML = ""
+            for (let tarea of tareas) {
+                tbody.innerHTML += `<tr>    
+    <td>${tarea.id}</td>
+    <td id="task">${tarea.nombre}</td>
+    <td><label for="opt1"></label>
+    <input type="checkbox" name="opt1" id="opt1" /> <button onclick="borrar(${tarea.completado})"> Eliminar </button></td>
+</tr>`;
+            }
+        }
+        btnAgregar.addEventListener("click", () => {
+                if (tareaInput.value == "") {
+                  alert("Debe agregar un valor");
+                  return;
+                }
+            const nuevaTarea = tareaInput.value
+            tareas.push({id: Date.now(), nombre: nuevaTarea, completado:false})
+            tareaInput.value = "" 
+            cuenta.innerHTML = 'Total Tareas: ' + tareas.length
+            renderTareas()   
+            
+        })
+
+        function borrar(id) {
+            const index = tareas.findIndex((ele) => ele.id == id)
+            tareas.splice(index, 1)
+            renderTareas()
+            cuenta.innerHTML = 'Total Tareas: ' + tareas.length
+        }
+
+    
+        opt1=document.querySelector('#opt1').checked
+            if(opt1 == true){
+            const task = document.querySelector("#task")
+            task.style.color = "red"
+        }
+
+
