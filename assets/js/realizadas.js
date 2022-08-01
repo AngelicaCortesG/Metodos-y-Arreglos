0083d7b4f4ef7171735ded7abdb2e582b917a327
Angelica <a.cortes.godoy@gmail.com>
2022-07-31 22:44:31 -0400

    edit html

--- assets/js/realizadas.js
+++ assets/js/realizadas.js
@@ -2,10 +2,9 @@ const cuentaRealizadas = document.querySelector("#realizadas")
 const realizadas = []
 function pintar () {
     // tareaAgregada = document.querySelector("#task");
-    check = document.querySelector("#opt1").checked;
+    check = document.querySelector(".opt").checked;
     if (check == true) {
-        tareaAgregada = document.querySelector("#task");
-        tareaAgregada.innerHTML = "caca"
+        tareaAgregada = document.querySelector(".task");
         tareaAgregada.style.color = "red"
         realizadas.push({id: Date.now(), nombre: nuevaTarea, completado:true})
         cuentaRealizadas.innerHTML = 'Total Realizadas: ' + realizadas.length
@@ -16,3 +15,4 @@ function pintar () {
     }
   };
 
+ 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,51 @@
-        const tareaInput = document.querySelector("#nuevaTarea")
-        const btnAgregar = document.querySelector("#agregarTarea")
-        const listaDeTareas = document.querySelector("#listaDeTareas")
-        const tbody = document.querySelector("tbody")
-        const cuenta = document.querySelector("#cuenta-tareas")
+const tareaInput = document.querySelector("#nuevaTarea")
+const btnAgregar = document.querySelector("#agregarTarea")
+const listaDeTareas = document.querySelector("#listaDeTareas")
+const tbody = document.querySelector("tbody")
+const cuenta = document.querySelector("#cuenta-tareas")
 
-        const tareas = []
+const tareas = [
+    // { id: 1, nombre: "Salir de compras", completado: false },
+    // { id: 2, nombre: "Estudiar Desafío", completado: false },
+    // { id: 3, nombre: "Hacer desafío", completado: false },
+]
+// const indiceTareas = tareas.findIndex( tarea => tarea.completado === false)
+// tareas.splice(indiceTareas, 0, completado: true)
 
-        function renderTareas () {
-            tbody.innerHTML = ""
-            for (let tarea of tareas) {
-                tbody.innerHTML += `<tr>    
+
+function renderTareas() {
+    let html =""
+    // tbody.innerHTML = ""
+    for (let tarea of tareas) {
+        html += `<tr>    
     <td>${tarea.id}</td>
     <td id="task">${tarea.nombre}</td>
-    <td><label for="opt1"></label>
-    <input type="checkbox" name="opt1" id="opt1" onChange="pintar()" /> <button onclick="borrar(${tarea.completado})"> Eliminar </button></td>
+    <td>
+    <input type="checkbox" id="opt1" onChange="pintar()" /> <button onclick="borrar(${tarea.completado})"> Eliminar </button></td>
 </tr>`;
-            }
-        }
-        btnAgregar.addEventListener("click", () => {
-                if (tareaInput.value == "") {
-                  alert("Debe agregar un valor");
-                  return;
-                }
-            const nuevaTarea = tareaInput.value
-            tareas.push({id: Date.now(), nombre: nuevaTarea, completado:false})
-            tareaInput.value = "" 
-            cuenta.innerHTML = 'Total Tareas: ' + tareas.length
-            renderTareas()   
-            
-        })
+    }
+    tbody.innerHTML = html
+}
+
+renderTareas()
+
+btnAgregar.addEventListener("click", () => {
+    if (tareaInput.value == "") {
+        alert("Debe agregar un valor");
+        return;
+    }
+    const nuevaTarea = tareaInput.value
+    tareas.push({ id: Date.now(), nombre: nuevaTarea, completado: false })
+    tareaInput.value = ""
+    cuenta.innerHTML = 'Total Tareas: ' + tareas.length
+    renderTareas()
+
+})
 
 function borrar(id) {
-            const index = tareas.findIndex((ele) => ele.id == id)
-            tareas.splice(index, 1)
-            renderTareas()
-            cuenta.innerHTML = 'Total Tareas: ' + tareas.length
-        }
+    const index = tareas.findIndex((ele) => ele.id == id)
+    tareas.splice(index, 1)
+    renderTareas()
+    cuenta.innerHTML = 'Total Tareas: ' + tareas.length
+}
 
-
-        
